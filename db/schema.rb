97a4d2ee3f2e45f4de9cf84f11ad63e33655cda5
LeeVaughn <dlvprogramming@gmail.com>
2020-12-23 10:12:56 -0800

    Create Micropost resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_040145) do
+ActiveRecord::Schema.define(version: 2020_12_23_180756) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.string "user__id"
+    t.string "integer"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
